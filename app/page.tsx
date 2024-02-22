1865a85f58b30de6370b146d1f960c1bbb80cbf6
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-22 02:53:33 -0700

    Created component for tictactoe, created css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Square from "./tictactoe";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         Dungeons & DragonBalls Wiki!
       </div>
-      <button className="square">X</button>
+      <Square />
     </main>
   );
 }
