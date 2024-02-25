7ba06b3a5f9ea5a3f08e73b26b65bc841dc7de0f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-02-25 02:10:44 -0700

    refactored globals.css, a lot of duplicate css from tut

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Board from "./tictactoe";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen px-10">
       <div className="flex flex-col items-center justify-between p-24">
         Dungeons & DragonBalls Wiki!
       </div>
