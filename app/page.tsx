3123260616273d3d48938ebecdbf817fb1db1afb
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-07 03:03:10 -0700

    more padding to homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,12 @@ export default function Home() {
         <h2 className="pb-3">
           Here you can learn all about the interactive experience that indulges in the world of DragonBall!
         </h2>
-        <h3>
+        <h3 className="pb-3">
           This application is being developed with the intent to record my notes and development of the assests of the world in a way that can translate to the actual game. If you are seeing this that means you are here before the official release of what is Dungeons and DragonBalls. <i> Lucky you -.-</i>
         </h3>
+        <h4>
+          Don't forget that you are so much happier when you are coding.
+        </h4>
 
       </div>
       {/* <Board /> */}
