b6171df8aeda88612ed92ecc960807863294cd9a
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-07 03:01:20 -0700

    set up page navigation for content categories

---
+++ app/forms/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export default function formsPage() {
+  return (
+    <main>
+      <div>
+        This is the forms page
+      </div>
+    </main>
+  )
+}
---
+++ app/origins/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export default function originsPage() {
+  return (
+    <main>
+      <div>
+        This is the Origins page
+      </div>
+    </main>
+  )
+}
