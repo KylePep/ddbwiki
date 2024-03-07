b5d3f06869dc81c9eb27424a62b62940ed3199dd
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-07 02:42:51 -0700

    Created links for expected pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 'use client'
+import Link from "next/link";
 import Board from "./tictactoe";
 
 export default function Home() {
   return (
     <main className="min-h-screen px-10">
-      <div className="flex flex-col items-center justify-between p-24 text-lg font-black">
+      <div className="flex flex-col items-center justify-between px-24 py-12 text-lg font-black">
+        <Link href={"/"}>
         Dungeons & DragonBalls Wiki!
+        </Link>
       </div>
-      <Board />
+      <div className="flex justify-between px-10 font-bold mb-10">
+        <Link href={"/"}>Races</Link>
+        <Link href={"/"}>Archetypes</Link>
+        <Link href={"/"}>Moves</Link>
+        <Link href={"/"}>Forms</Link>
+      </div>
+      {/* <Board /> */}
     </main>
   );
 }
