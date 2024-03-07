7be8513c1db5c90ed9ba384a144cf0c36d08804e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-07 02:49:09 -0700

    Home page content

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,18 @@ export default function Home() {
         <Link href={"/"}>Moves</Link>
         <Link href={"/"}>Forms</Link>
       </div>
+      <div className="flex flex-col items-center">
+        <h1 className="pb-4">
+        Welcome to the Dungeons and DragonBalls Wiki!
+        </h1>
+        <h2 className="pb-3">
+          Here you can learn all about the interactive experience that indulges in the world of DragonBall!
+        </h2>
+        <h3>
+          This application is being developed with the intent to record my notes and development of the assests of the world in a way that can translate to the actual game. If you are seeing this that means you are here before the official release of what is Dungeons and DragonBalls. <i> Lucky you -.-</i>
+        </h3>
+
+      </div>
       {/* <Board /> */}
     </main>
   );
