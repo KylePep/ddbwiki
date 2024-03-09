20db316502df96218a025cb323feee643ecd2926
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-09 02:41:34 -0700

    adjusted special resources styling

--- app/components/SpecialResources.tsx
+++ app/components/SpecialResources.tsx
@@ -7,10 +7,28 @@ export default function SpecialResources() {
       <h1>Characters have access to special resources depending on archetypes and stats. The universal resource is the <i>SPARKING METER</i> </h1>
       <h2> Archetype Resources:</h2>
       <ul>
-        <li>Heroic Counters - helps this archetype rise to the occasion, usually to save an ally.</li>
-        <li>Prank Counters - used to distract enemies or interfere with plans.</li>
-        <li>Pride Counters - helps prevent certain status effects and withstand deadly encounters.</li>
-        <li>Rage Counters - react to danger with heightened damage capabilities.</li>
+        <li> <div className='font-medium'>Heroic Counters
+        </div> 
+        <p> - helps this archetype rise to the occasion, usually to save an ally.</p>
+        </li>
+        <li><div className='font-medium'>Prank Counters
+        </div>
+        <p>
+           - used to distract enemies or interfere with plans.
+          </p>
+           </li>
+        <li><div className='font-medium'>Pride Counters
+        </div> 
+        <p>
+          - helps prevent certain status effects and withstand deadly encounters.
+          </p>
+          </li>
+        <li><div className='font-medium'>Rage Counters
+        </div>
+         <p>
+           - react to danger with heightened damage capabilities.
+          </p>
+           </li>
       </ul>
     </div>
   )
