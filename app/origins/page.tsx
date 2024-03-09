7f7752b143900d5a3306be2f4ff65c310fdcc96f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-09 02:15:01 -0700

    added content to Origins page

--- app/origins/page.tsx
+++ app/origins/page.tsx
@@ -6,7 +6,24 @@ export default function originsPage() {
   return (
     <main>
       <div>
-        This is the Origins page
+        <h1>
+        Origins are the combination of race and background.
+        </h1>
+          <div>
+            <h2 className='font-bold'>Saiyan</h2>
+            <p>A Race of space barbarians with the power to transform into Great Apes. Huge appetites</p>
+            <p>Racial traits: +Power, -Spirit, Transformation(Ape-Form)</p>
+            <h2 className='font-bold'>Half Saiyan</h2>
+            <p>The result of a human and a saiyan, boasting many of the same strengths as a saiyan but with interest in battle. Huge Appetites </p>
+            <p>Racial traits: +Ki, -Toughness, Transformation(Ape-Form)</p>
+            <h2 className='font-bold'>Earthling</h2>
+            <p>Humans capable of harnessing Ki</p>
+            <p>Racial Traits: +Spirit, -Ki, Technology(item)</p>
+            <h2 className='font-bold'>Namekian</h2>
+            <p>Humanoids with plant and slug-like characteristics, including green skin and antennae. Can survive on only water.</p>
+            <p>Racial traits: +Toughness, -Agility, Regernation(Limbs)</p>
+            <h3>***More can be added, but these are the starters***</h3>
+          </div>
       </div>
     </main>
   )
