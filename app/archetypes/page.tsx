8660791a174f014256e2d1d1f0fdfd6d3e48c02d
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-09 02:30:54 -0700

    Added content to forms page

--- app/archetypes/page.tsx
+++ app/archetypes/page.tsx
@@ -8,16 +8,16 @@ export default function archetypesPage() {
       <div>
         <h1 className='mb-4'>The classes of Dungeons & DragonBalls</h1>
         <h2 className='font-bold'>Saiyan Warrior</h2>
-        <p>( Goku | Vegeta )</p>
+        <p> Goku | Vegeta </p>
         <p>The battle hungry archetype geared for high intensity combat.</p>
         <h2 className='font-bold'>Support</h2>
-        <p>( Hercule | Chiaotzu | Krillin)</p>
+        <p> Hercule | Chiaotzu | Krillin</p>
         <p>The comic relief archetype, helpful distractions and buff to the party.</p>
         <h2 className='font-bold'>Striker</h2>
-        <p>( Gohan | Trunks)</p>
+        <p> Gohan | Trunks</p>
         <p>An archetype capable of altering the battlefield and negating villains abilities</p>
         <h2 className='font-bold'>Tank</h2>
-        <p>( Piccolo | Tien )</p>
+        <p> Piccolo | Tien </p>
         <p>The sturdiest archetype, capable of absorbing large amounts of damage, regenerating from it or saving others.</p>
       </div>
     </main>
--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -6,7 +6,17 @@ export default function formPage() {
   return (
     <main>
       <div>
-        This is the forms page
+        <h1>Transformations are a change to a characters state that is reflected in the changing of stats or the manifestation fo passive or active abilities</h1>
+        <div>Transformations can be an aura, the alteration of a physical characteristic or a full body change</div>
+        <h2 className='font-bold'>Auras</h2>
+        <p>potential unlocked</p>
+        <p>Mostly consist of a slight boost in power at the cost of being slightly more taxing on a stat or resource</p>
+        <h2 className='font-bold'>Alterations</h2>
+        <p>Super Saiyan</p>
+        <p>Alterations are in the middle, giving a more powerful temporary boost while being maintained by a resource</p>
+        <h2 className='font-bold'>Forms</h2>
+        <p>Great Ape</p>
+        <p>Forms are at the high end. These changes have the most risk for reward, if the user {`can't`} keep up with the demand for a resource, exhaustion can set in fast and leave the user vulnerable.</p>
       </div>
     </main>
   )
