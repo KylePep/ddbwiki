32c4e3df2f9211af5eb3847cd818eb33473d54b9
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-10 03:22:11 -0600

    Added content to moves page

--- app/moves/page.tsx
+++ app/moves/page.tsx
@@ -6,7 +6,21 @@ export default function movesPage() {
   return (
     <main>
       <div>
-        This is the Moves page
+        <h1>A list of moves in D&DB</h1>
+        <ul>
+          <li>
+            <h2>Name: Kamahameha</h2>
+            <p>Description: An energy wave attack.</p>
+          </li>
+          <li>
+            <h2>Name: Special Beam Cannon</h2>
+            <p>Description: A concentrated beam attack that takes a long time to charge.</p>
+          </li>
+          <li>
+            <h2>Name: Double Sunday</h2>
+            <p>Description: An enery wave attack capable of targeting two opponents.</p>
+          </li>
+        </ul>
       </div>
     </main>
   )
