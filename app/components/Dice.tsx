f87e96aad2394c8d14b2f548ba11a4e49e6909cc
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-10 04:07:31 -0600

    Dice component returns a random value between 1 and die selected

--- app/components/Dice.tsx
+++ app/components/Dice.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Dice() {
+  const [selectedDie, setSelectedDie] = useState('D4');
+  const [result, setResult] = useState(null);
+
+  const rollDie = () => {
+    const maxNumber = parseInt(selectedDie.slice(1)); // Extract the number from selectedDie (e.g., D4 -> 4)
+    const randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+    setResult(randomNumber);
+  };
+
   return (
     <>
-      <div className='grid grid-rows-1 grid-flow-col gap-4 bg-gray-200 '>
-        <div className='min-h-20 bg-red-500'>1</div>
-        <div className='grid grid-rows-6 grid-flow-col'>
-          <div>D4</div>
-          <div>D6</div>
-          <div>D8</div>
-          <div>D10</div>
-          <div>D12</div>
-          <div>D20</div>
+      <p>
+        Function: Select a Die type and roll it. Get the result back listed as Result. The selected Dice will be outlined in orange.
+      </p>
+
+      <div className='grid grid-rows-1 grid-flow-col gap-2 bg-gray-200 rounded my-4 p-2'>
+
+        <div className='flex flex-col items-center justify-center min-h-20'>
+          <button className='bg-gray-100 hover:bg-gray-200 px-4 py-1 mb-3 rounded-md' onClick={rollDie}>
+            Roll
+          </button>
+          <div>Result: {result !== null ? result : 'X'}</div>
         </div>
 
+        <div className='grid grid-rows-3 gap-1 grid-flow-col'>
+          {['D4', 'D6', 'D8', 'D10', 'D12', 'D20'].map(dieType => (
+            <button
+              key={dieType}
+              className={`bg-gray-400 hover:bg-gray-300 py-1 text-center rounded ${selectedDie === dieType ? 'border-solid border-2 border-orange-500' : ''}`}
+              onClick={() => setSelectedDie(dieType)}
+            >
+              {dieType}
+            </button>
+          ))}
+        </div>
       </div>
     </>
-  )
+  );
 }
