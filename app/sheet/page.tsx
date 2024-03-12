61890494a4b2e31ffd969b25bb5a05fe9ec4d09c
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-12 03:15:14 -0600

    More content to form

--- app/sheet/page.tsx
+++ app/sheet/page.tsx
@@ -4,25 +4,26 @@ export default function page() {
   return (
     <div>
       <div>
-        Build a character
-        <div className='bg-gray-100 min-h-96 px-4 py-2 rounded'>
+        <div className='bg-gray-100  px-8 py-4 rounded'>
 
-
-          <form className='grid grid-rows-1 grid-flow-col'>
-            
-            <div>
-              <p>
-                Character Sheet
+              <p className='mb-5 text-lg font-bold'>
+                Dungeons & DragonBalls Character sheet
               </p>
+          <form className='grid grid-rows-1 grid-flow-col'>
+            <div>
+            <div className='flex gap-2'>
+
+            <div>
               <div>
-                <label htmlFor="name">Name </label>
+                <label htmlFor="name" className='font-bold'>Name </label>
                   <div>
-                    <input type="text" name="name" id="name"  />
+                    <input className='ps-2' type="text" name="name" id="name" minLength={1} maxLength={20}  placeholder='Kakarot'/>
                 </div>
               </div>
+            </div>
 
-              <div>
-                <label htmlFor="name">Origin </label>
+            <div>
+                <label htmlFor="name" className='font-bold'>Origin </label>
                   <div>
                     <select name="origin" id="origin">
                       <option value="saiyan">Saiyan</option>
@@ -34,7 +35,7 @@ export default function page() {
               </div>
 
               <div>
-                <label htmlFor="name">Archetype </label>
+                <label htmlFor="name" className='font-bold'>Archetype </label>
                   <div>
                     <select name="archetype" id="archetype">
                       <option value="warrior">Warrior</option>
@@ -45,29 +46,85 @@ export default function page() {
                 </div>
               </div>
             </div>
-
-            <div className='flex flex-col gap-2'>
-              -STATS-
-              <div className='grid grid-rows-3 grid-flow-col gap-2'>                
+              <div>
+                <div>
+                    <p className='font-bold'>Moves</p>
+                  <div className='bg-white me-10 py-2'>
+                    <ul>
+                      <li>Ki Blast</li>
+                      <li>Kamehameha</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                    <p className='font-bold'>Forms</p>
+                  <div className='bg-white me-10 py-2'>
+                    <ul>
+                      <li>Great Ape</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                    <p className='font-bold'>Inventory</p>
+                  <div className='bg-white me-10 py-2'>
+                    <ul>
+                      <li>Scouter</li>
+                      <li>Power Pole</li>
+                    </ul>
+                  </div>
+                </div>
                 <div className='flex flex-col'>
+                  <label className='font-bold' >Description</label>
+                  <textarea className='resize-none me-10' name="description" id="description" cols={30} rows={4}></textarea>
+                </div>
+              </div>
+            </div>
+
+
+            <div className='flex flex-col border-solid border-s-2 border-black-500 '>
+              <p  className='text-center font-bold'>
+              Stats
+              </p>
+              
+              <div className='grid grid-rows-3 grid-flow-col gap-2'>                
+                <div className='flex flex-col w-10 m-auto'>
                   <label htmlFor="name">Spirit </label>
                   <input type="number" name="spirit" id="spirit" min={1} max={5}  />
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-10 m-auto'>
                   <label htmlFor="name">Power </label>
-                  <input type="number" name="power" id="power" value={3} />
+                  <input type="number" name="power" id="power" min={1} max={5} />
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-10 m-auto'>
                   <label htmlFor="name">Toughness </label>
-                  <input type="number" name="toughness" id="toughness" value={3} />
+                  <input type="number" name="toughness" id="toughness" min={1} max={5} />
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-10 m-auto'>
                   <label htmlFor="name">Ki </label>
-                  <input type="number" name="ki" id="ki" value={3} />
+                  <input type="number" name="ki" id="ki" min={1} max={5} />
                 </div>
-                <div className='flex flex-col'>
+                <div className='flex flex-col w-10 m-auto'>
                   <label htmlFor="name">Agility </label>
-                  <input type="number" name="agility" id="agility" value={3} />
+                  <input type="number" name="agility" id="agility" min={1} max={5} />
+                </div>
+              </div>
+              <div>
+                <p className='text-center font-bold'>Equipment</p>
+                <div>
+                    <p className='text-center'>Head</p>
+                  <div className='bg-white me-10 py-2'>
+                    <ul>
+                      <li>Saiyan Scouter</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                    <p className='text-center'>Body</p>
+                  <div className='bg-white me-10 py-2'>
+                    <ul>
+                      <li>Saiyan Armor</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
