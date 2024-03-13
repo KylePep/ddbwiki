b339980d4a6046794f21d5fdee0d24c343b0d1d6
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-13 07:49:01 -0600

    Created app content inside of data.json

--- shared/constants/index.js
+++ shared/constants/index.js
@@ -1,3 +1,8 @@
 import * as data from './data.json'
 
 export const CHARACTER_TYPES = data.CHARACTER_TYPES
+export const ORIGIN_TYPES = data.ORIGIN_TYPES
+export const ARCHETYPE_TYPES = data.ARCHETYPE_TYPES
+export const FORM_TYPES = data.FORM_TYPES
+export const ITEM_TYPES = data.ITEM_TYPES
+export const MOVE_TYPES = data.MOVE_TYPES
