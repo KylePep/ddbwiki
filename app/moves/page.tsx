269054632f9affb89d45938b425b1977c7cd90ea
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-13 07:56:57 -0600

    Reformatted moves to support data from shared folder

--- app/moves/page.tsx
+++ app/moves/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { MOVE_TYPES } from "../../shared/constants/index";
 
 import React from 'react'
 
@@ -8,18 +9,14 @@ export default function movesPage() {
       <div>
         <h1 className='mb-3'>A list of moves in D&DB</h1>
         <ul>
-          <li className='mb-2'>
-            <h2>Name: Kamahameha</h2>
-            <p>Description: An energy wave attack.</p>
+          {MOVE_TYPES.map((move: any)=>(
+          <li key={move.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
+                <h2>Title: {move.title}</h2>
+                <h3>Type: {move.type}</h3>
+                <h4>Style: {move.style}</h4>
+                <p>Description {move.description}</p>
           </li>
-          <li className='mb-2'>
-            <h2 >Name: Special Beam Cannon</h2>
-            <p>Description: A concentrated beam attack that takes a long time to charge.</p>
-          </li>
-          <li className='mb-2'>
-            <h2>Name: Double Sunday</h2>
-            <p>Description: An enery wave attack capable of targeting two opponents.</p>
-          </li>
+              ))}
         </ul>
       </div>
     </main>
