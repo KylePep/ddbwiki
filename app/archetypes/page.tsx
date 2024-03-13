9a4cfac39a3100026f36487ad811d12fee5957b9
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-13 08:00:30 -0600

    Connected archs to shared folder

--- app/archetypes/page.tsx
+++ app/archetypes/page.tsx
@@ -1,25 +1,21 @@
 'use client'
 
+import { ARCHETYPE_TYPES } from '@/shared/constants'
 import React from 'react'
 
 export default function archetypesPage() {
   return (
     <main>
-      <div>
-        <h1 className='mb-4'>The classes of Dungeons & DragonBalls</h1>
-        <h2 className='font-bold'>Saiyan Warrior</h2>
-        <p> Goku | Vegeta </p>
-        <p>The battle hungry archetype geared for high intensity combat.</p>
-        <h2 className='font-bold'>Support</h2>
-        <p> Hercule | Chiaotzu | Krillin</p>
-        <p>The comic relief archetype, helpful distractions and buff to the party.</p>
-        <h2 className='font-bold'>Striker</h2>
-        <p> Gohan | Trunks</p>
-        <p>An archetype capable of altering the battlefield and negating villains abilities</p>
-        <h2 className='font-bold'>Tank</h2>
-        <p> Piccolo | Tien </p>
-        <p>The sturdiest archetype, capable of absorbing large amounts of damage, regenerating from it or saving others.</p>
-      </div>
+          <h1 className='mb-4'>The classes of Dungeons & DragonBalls</h1>
+      <ul>
+      {ARCHETYPE_TYPES.map((arch: any)=>(
+          <li key={arch.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
+                <h2 className='font-bold'>{arch.title}</h2>
+                <h3>{arch.examples}</h3>
+                <p>Description {arch.description}</p>
+          </li>
+              ))}
+      </ul>
     </main>
   )
 }
