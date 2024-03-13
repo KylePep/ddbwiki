a32adbfb6a21a7006543870ed40d6e82b77bb579
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-13 08:03:48 -0600

    Connected Origins to shared folder data

--- app/origins/page.tsx
+++ app/origins/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ORIGIN_TYPES } from '@/shared/constants'
 import React from 'react'
 
 export default function originsPage() {
@@ -9,21 +10,17 @@ export default function originsPage() {
         <h1>
         Origins are the combination of race and background.
         </h1>
-          <div>
-            <h2 className='font-bold'>Saiyan</h2>
-            <p>A Race of space barbarians with the power to transform into Great Apes. Huge appetites</p>
-            <p>Racial traits: +Power, -Spirit, Transformation(Ape-Form)</p>
-            <h2 className='font-bold'>Half Saiyan</h2>
-            <p>The result of a human and a saiyan, boasting many of the same strengths as a saiyan but with interest in battle. Huge Appetites </p>
-            <p>Racial traits: +Ki, -Toughness, Transformation(Ape-Form)</p>
-            <h2 className='font-bold'>Earthling</h2>
-            <p>Humans capable of harnessing Ki</p>
-            <p>Racial Traits: +Spirit, -Ki, Technology(item)</p>
-            <h2 className='font-bold'>Namekian</h2>
-            <p>Humanoids with plant and slug-like characteristics, including green skin and antennae. Can survive on only water.</p>
-            <p>Racial traits: +Toughness, -Agility, Regernation(Limbs)</p>
+          <ul>
+          {ORIGIN_TYPES.map((origin: any)=>(
+          <li key={origin.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
+                <h2 className='font-bold'>{origin.title}</h2>
+                <p>Description: {origin.description}</p>
+                <h3>Racial traits: {origin.racialTraits}</h3>
+                <h4>Transformations: {origin.forms}</h4>
+          </li>
+              ))}
             <h3>***More can be added, but these are the starters***</h3>
-          </div>
+          </ul>
       </div>
     </main>
   )
