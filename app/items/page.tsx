13cec8acac8f490f5415f8e9a75c59eed8f8d954
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-14 09:23:45 -0600

    Connected items page to data.json

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,9 +1,22 @@
+import { ITEM_TYPES } from '@/shared/constants'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      Items Page
-    </div>
+<main>
+<div>
+        <h1 className='mb-3'>A list of items in D&DB</h1>
+        <ul>
+          {ITEM_TYPES.map((move: any)=>(
+          <li key={move.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
+                <h2 className="font-bold">{move.title}</h2>
+                <h3>Type: {move.type}</h3>
+                <h4>Sub Type: {move.subType}</h4>
+                <p>Description {move.description}</p>
+          </li>
+              ))}
+        </ul>
+      </div>
+</main>
   )
 }
