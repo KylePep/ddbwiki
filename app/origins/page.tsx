8985382a46f224d88e0ba008fbb42eedcdf7a297
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-15 21:34:23 -0600

    Updated origin data objects

--- app/origins/page.tsx
+++ app/origins/page.tsx
@@ -13,10 +13,16 @@ export default function originsPage() {
           <div>
           {ORIGIN_TYPES.map((origin: any)=>(
           <div key={origin.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
-                <h2 className='font-bold'>{origin.title}</h2>
-                <p><span className='font-semibold'>Description: </span> {origin.description}</p>
-                <h3><span className='font-semibold'>Origin traits: </span> {origin.racialTraits}</h3>
+                <h2 className='font-bold mb-1'>{origin.title}</h2>
+                <p className='capitalize'><span className='font-semibold'>Description: </span> {origin.description}</p>
+                <h3 className='capitalize'><span className='font-semibold'>Origin traits: </span> + {origin.positiveTrait} | - {origin.negativeTrait}</h3>
                 <h4><span className='font-semibold'>Transformations: </span> {origin.forms}</h4>
+                <h5 className='font-semibold'>Origin Moves:</h5>
+                <ul>
+                  {origin.moves.map((move: any) => (
+                    <li key={move}>{move}</li>
+                  ))}
+                </ul>
           </div>
               ))}
             <h3>***More can be added, but these are the starters***</h3>
