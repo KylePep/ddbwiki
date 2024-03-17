614f0e0da8149239da025c27bc2c7881fbdef371
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-16 20:25:28 -0600

    Conditionally rendering move title based on moves id number

--- app/origins/page.tsx
+++ app/origins/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 
 import { ORIGIN_TYPES } from '@/shared/constants'
+import { MOVE_TYPES } from '@/shared/constants'
 import React from 'react'
 
 export default function originsPage() {
+
+  const moveName = ((moveNumber: number)=>{
+    const move =  MOVE_TYPES.find((m) => m.id == moveNumber)
+    return move?.title
+  })
+
   return (
     <main>
       <div>
@@ -17,12 +24,16 @@ export default function originsPage() {
                 <p className='capitalize'><span className='font-semibold'>Description: </span> {origin.description}</p>
                 <h3 className='capitalize'><span className='font-semibold'>Origin traits: </span> + {origin.positiveTrait} | - {origin.negativeTrait}</h3>
                 <h4><span className='font-semibold'>Transformations: </span> {origin.forms}</h4>
-                <h5 className='font-semibold'>Origin Moves:</h5>
-                <ul>
-                  {origin.moves.map((move: any) => (
-                    <li key={move}>{move}</li>
-                  ))}
+                { origin.moves.length != 0 && (
+                  <>
+                  <h5 className='font-semibold'>Origin Moves:</h5>
+                  <ul>
+                    {origin.moves.map((move: any) => (
+                      <li key={move}>{moveName(move)}</li>
+                    ))}
                 </ul>
+                  </>
+                )}
           </div>
               ))}
             <h3>***More can be added, but these are the starters***</h3>
