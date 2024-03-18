3c176c2dfd4894c29d415a123478bd27f4b29a9f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-18 02:07:45 -0600

    Added to todo list

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,17 @@ export default function Home() {
           <p className="mb-3 text-xl font-bold">ToDo List:</p>
           <ul className="list-decimal">
             <li>
+              <p className="font-bold">
+              Create a filter component capable of being used on several pages
+              </p>
+              <ul className="list-disc">
+                <li>LIST_TYPE</li>
+                <li>Filter Categories</li>
+                <li>Filter Options</li>
+                <li>Return new list</li>
+              </ul>
+            </li>
+            <li>
               <p className="font-bold">Enemies</p>
               <ul className="list-disc">
                 <li>Bosses</li>
