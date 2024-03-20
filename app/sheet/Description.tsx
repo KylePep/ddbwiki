0c7615946da935b433e56199f6629fdc6e2ca45e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-20 12:09:16 -0600

    A description of your character can be autogenerated based on selected attributes

--- app/sheet/Description.tsx
+++ app/sheet/Description.tsx
@@ -5,10 +5,22 @@ interface formProps{
 }
 
 export default function Description({form, handleChange}:formProps) {
+  const autoDescription = `${form.name} is a ${form.origin} ${form.archetype}.`
+
+  const generateDescription = () => {
+    form.description = autoDescription;
+    handleChange({
+      target: {
+        name: 'description',
+        value: autoDescription
+      }
+    } as React.ChangeEvent<HTMLTextAreaElement>);
+  }
+
   return (
     <div className='flex flex-col'>
-    <label className='font-bold' >Description</label>
-    <textarea onChange={handleChange} value={form.description} className='resize-none me-10 px-4' name="description" id="description" cols={30} rows={4} placeholder='Provide a brief description of your character here...'></textarea>
+    <label onClick={generateDescription} className='font-bold'>Description <span><button className='bg-blue-300 rounded px-1 font-medium hover:text-white'>Generate</button></span></label>
+    <textarea onChange={handleChange} value={form.description} className='resize-none me-10 px-4' name="description" id="description" cols={30} rows={4}  placeholder='Provide a brief description of your character here...'></textarea>
   </div>
   )
 }
