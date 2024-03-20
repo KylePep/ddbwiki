3e142970e155900dd5e4e9d880618e9a3623fb83
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-20 06:05:03 -0600

    imported FilterList into enemies page

--- app/enemies/page.tsx
+++ app/enemies/page.tsx
@@ -1,13 +1,18 @@
+'use client'
 import { ENEMY_TYPES } from '@/shared/constants'
-import React from 'react'
+import React, { useState } from 'react'
+import FilterList from '../components/FilterList'
 
 export default function page() {
+  const [list, setList] = useState(ENEMY_TYPES)
+
   return (
     <main>
       <div>
         <h1 className='mb-3'>A list of enemies in D&DB</h1>
+        <FilterList LIST_TYPE={ENEMY_TYPES} list={list} setList={setList} categoryFilterArr={["type.basic", "type.boss"]}/>
         <div>
-          {ENEMY_TYPES.map((move: any)=>(
+          {list.map((move: any)=>(
           <div key={move.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
                 <h2 className="font-bold">{move.title}</h2>
                 <h3><span className='font-semibold'>Type: </span>{move.type}</h3>
