a9f7f2e4cc75a22599a1c572ebbd505b45b929e3
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-25 02:56:08 -0600

    Added point of interest content to World

--- app/world/page.tsx
+++ app/world/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
       <h1 className='mb-3 text-lg'>The world of Dungeons & DragonBalls. Here you can learn about locations such as planets and points of interest.</h1>
       <div>
         <h2 className='font-bold text-md'>Locations</h2>
-        <ul>
+        <ul className='mb-3'>
           <li>Planet Earth</li>
           <li>Planet Namek</li>
           <li>Planet New Namek</li>
@@ -31,7 +31,48 @@ export default function page() {
         </ul>
         <h2 className='font-bold text-md'>Points of Interest</h2>
         <ul>
-          <li>Hyperbolic Time Chamber</li>
+          <li>Cell Games Arena</li>
+          <li>Kame House</li>
+          <li>Check-In Station</li>
+          <li>Snake Way</li>
+            <ul>
+              <li>Princess {`Snake's`} palace</li>
+            </ul>
+          <li>Mount Paozu</li>
+            <ul>
+              <li>Diablo Desert</li>
+                <ul>
+                  <li>{`Yamcha's`} Hideout</li>
+                  <li>{`Pilaf's`} Castle</li>
+                </ul>
+              </ul>
+            <ul>
+              <li>East District</li>
+              <ul>
+                <li>Grandpa {`Gohan's`} House</li>
+                <li>{`Goku's`} House</li>
+              </ul>
+            </ul>
+          <li>Sacred Land of Korin</li>
+            <ul>
+              <li>Hermit Spring</li>
+              <li>Korin Tower</li>
+              <li>The Lookout</li>
+                <ul>
+                  <li>Hyperbolic Time Chamber</li>
+                  <li>Pendulum Room</li>
+                </ul>
+            </ul>
+          <li>Satan City</li>
+            <ul>
+              <li>Satan House</li>
+              <li>Orange Star High School</li>
+              <li>Satan City Primary School</li>
+            </ul>
+          <li>West City</li>
+            <ul>
+              <li>Capsule Corporation</li>
+            </ul>
         </ul>
       </div>
     </main>
