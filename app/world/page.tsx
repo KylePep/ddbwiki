c5af3fa95574c2d2520df8b1349eedd680c0f67c
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-25 02:44:02 -0600

    added content to World page

---
+++ app/world/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+      <h1 className='mb-3 text-lg'>The world of Dungeons & DragonBalls. Here you can learn about locations such as planets and points of interest.</h1>
+      <div>
+        <h2 className='font-bold text-md'>Locations</h2>
+        <ul>
+          <li>Planet Earth</li>
+          <li>Planet Namek</li>
+          <li>Planet New Namek</li>
+          <li>Other World</li>
+          <li>Planet Vegeta</li>
+          <li>Planet New Vegeta</li>
+          <li>King {`Kai's`} Planet</li>
+          <li>Planet New Vegeta</li>
+          <li>Big Gete Star</li>
+          <li>{`Beerus'`} Planet</li>
+          <li>Cereal</li>
+          <li>Demon Realm</li>
+          <li>Grand {`Kai's`} Planet</li>
+          <li>Hera</li>
+          <li>Konats</li>
+          <li>Makyo Star</li>
+          <li>Metamor</li>
+          <li>Planet Cruiser</li>
+          <li>Sadala</li>
+          <li>Vampa</li>
+          <li>Yardrat</li>
+        </ul>
+        <h2 className='font-bold text-md'>Points of Interest</h2>
+        <ul>
+          <li>Hyperbolic Time Chamber</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
