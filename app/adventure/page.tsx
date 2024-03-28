2067dee999624bee4221ba875462924dcad902ea
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-28 02:49:26 -0600

    added content to adventure page

--- app/adventure/page.tsx
+++ app/adventure/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function page() {
   return (
     <main>
-      Adventure!
+      <h1>Here you can find out about the Adventures that await you in Dungeons & DragonBalls!</h1>
+      <div className='bg-white rounded'>
+        <h2>Adventure</h2>
+        <h3>An adventure is a series of events that will help you and up to 2 friends explore the world of DragonBall. In an adventure you will be faced with tough decisions and powerful enemies! With the sparking power of the Zfighters you can rise to the occasion and become the strongest!</h3>
+      </div>
     </main>
   )
 }
