b8c797117a32ccb4ea8fb4236243435b70897355
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-28 02:54:41 -0600

    more content to adventure

--- app/adventure/page.tsx
+++ app/adventure/page.tsx
@@ -7,6 +7,14 @@ export default function page() {
       <div className='bg-white rounded'>
         <h2>Adventure</h2>
         <h3>An adventure is a series of events that will help you and up to 2 friends explore the world of DragonBall. In an adventure you will be faced with tough decisions and powerful enemies! With the sparking power of the Zfighters you can rise to the occasion and become the strongest!</h3>
+        <p>An adventure is an automated experience. 
+          Think of it like a series of rooms. The simplest of adventures would be a one room that leads to a single room that then leads to a single room.
+        </p>
+        <h4>Adventures</h4>
+        <p>Although this is an expectable way to play it {`wouldn't`} be the most fun.</p>
+        <p>A more complex adventure can feature rooms with multiple doors, that lead to rooms with even more doors.</p>
+        <h4>Rooms</h4>
+        <p>Within a room</p>
       </div>
     </main>
   )
