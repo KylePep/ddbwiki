d692dedf7aa7409e5a9d661edb671316f223ab86
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-03-30 02:09:26 -0600

    Created adventureData.json

--- shared/constants/index.js
+++ shared/constants/index.js
@@ -1,5 +1,6 @@
 import * as data from './data.json'
 import * as moveData from './moveData.json'
+import * as adventureData from './adventureData.json'
 
 export const {
   CHARACTER_TYPES,
@@ -11,3 +12,4 @@ export const {
 } = data;
 
 export const { MOVE_TYPES } = moveData
+export const { ADVENTURE_TYPES } = adventureData
