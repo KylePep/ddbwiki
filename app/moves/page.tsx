a31ec2784715de3d0b6a34f2c037b24395517e15
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-04-01 07:03:54 -0600

    commit?

--- app/moves/page.tsx
+++ app/moves/page.tsx
@@ -12,7 +12,7 @@ export default function movesPage() {
     <main>
       <div>
         <h1 className='mb-3'>The moves a character can perform on their adventure in Dungeons and DragonBalls!</h1>
-          <FilterList LIST_TYPE={MOVE_TYPES} list={list} setList={setList} categoryFilterArr={["type.attack", "type.support", "style.physical", "style.energy"]} />
+          <FilterList LIST_TYPE={MOVE_TYPES} list={list} setList={setList} categoryFilterArr={["type.attack", "type.support", "subType.physical", "subType.energy"]} />
         <div>
           {list.map((move: any)=>(
           <div key={move.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
