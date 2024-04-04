47a95a2d3979e3d8eb05a398168cd8cb6cd4377f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-04-04 02:53:52 -0600

    Moved item data into its own data object

--- shared/constants/index.js
+++ shared/constants/index.js
@@ -2,15 +2,16 @@ import * as data from './data.json'
 import * as moveData from './moveData.json'
 import * as adventureData from './adventureData.json'
 import * as enemyData from './enemyData.json'
+import * as itemData from './itemData.json'
 
 export const {
   CHARACTER_TYPES,
   ORIGIN_TYPES,
   ARCHETYPE_TYPES,
-  FORM_TYPES,
-  ITEM_TYPES
+  FORM_TYPES
 } = data;
 
 export const { MOVE_TYPES } = moveData
 export const { ADVENTURE_TYPES } = adventureData
 export const { ENEMY_TYPES } = enemyData
+export const { ITEM_TYPES } = itemData
