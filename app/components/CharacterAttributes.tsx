d84c37024f1d0fade425ee0f9f39ffb586696093
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-04-09 05:41:16 -0600

    refactored stats and clarified their function

--- app/components/CharacterAttributes.tsx
+++ app/components/CharacterAttributes.tsx
@@ -4,11 +4,11 @@ export default function CharacterAttributes() {
   return (
     <div className=' bg-white p-4 rounded'>
       <h1 className='text-lg font-bold mb-4'>CHARACTER ATTRIBUTES</h1>
-      <p className='text-lg font-bold mb-2'>Health:</p>
-      <ul className='list-disc'>
+      <p className='font-bold mb-2'>Health:</p>
+      <ul className='list-disc text-sm'>
         <li>When health is reduced to zero, a character is out of combat unless they are recovered by a healing item such as a Senzu Bean.</li>
         <li>While out of combat, a character {`can't`} use attacks but can use some support moves.</li>
-        <li></li>
+        <li>Health can be recovered by healing items, but {`can't`} be raised above {`it's`} max.</li>
       </ul>
     </div>
   )
