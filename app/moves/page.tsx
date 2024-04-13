29f16af10988fd8f2e5387ef9fc34a0f74236824
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-04-13 02:34:16 -0600

    for better data representation, moved move details onto a move details page

--- app/moves/page.tsx
+++ app/moves/page.tsx
@@ -3,6 +3,7 @@ import { MOVE_TYPES } from "../../shared/constants/index";
 
 import React, { useState } from 'react'
 import FilterList from "../components/FilterList";
+import Link from "next/link";
 
 
 export default function movesPage() {
@@ -11,46 +12,24 @@ export default function movesPage() {
   return (
     <main>
       <div>
-        <h1 className='mb-3 bg-white rounded p-4'>The moves a character can perform on their adventure in Dungeons and DragonBalls!</h1>
+        <h1 className='mb-3 bg-gray-100 rounded p-4'>The moves a character can perform on their adventure in Dungeons and DragonBalls!</h1>
           <FilterList LIST_TYPE={MOVE_TYPES} list={list} setList={setList} categoryFilterArr={["type.attack", "type.support", "subType.physical", "subType.energy"]} />
           {/* Minimum view */}
         <div>
           {list.map((move: any)=>(
-          <div key={move.id} className='grid grid-cols-2 gap-2 bg-gray-100 rounded-md px-4 py-2 mb-2'>
+          <div key={move.id} className='grid grid-cols-3 gap-2 bg-gray-100 rounded-md px-4 py-2 mb-2'>
             <div>
                 <h2 className="font-bold">{move.displayName}</h2>
                   <h3 className='my-2'><span className='font-semibold'>Type: </span>{move.type} <span> - {move.subType}</span></h3>
             </div>
                   <p className="mb-2">{move.description}</p>
+                  <div className="flex justify-center items-center">
+                  <Link href={`moves/${move.name}`} className="font-semibold hover:text-white bg-gray-400 rounded p-1"> Details</Link>
+                  </div>
 
           </div>
               ))}
         </div>
-        {/* Maximum view */}
-        {/* <div>
-          {list.map((move: any)=>(
-          <div key={move.id} className='bg-gray-100 rounded-md px-4 py-2 mb-2'>
-                <h2 className="font-bold">{move.displayName}</h2>
-                  <p className="mb-2">{move.description}</p>
-                  <p className="mb-2 mx-8">{move.extraDescription}</p>
-
-                <div>
-                  <p className="border-solid border-2 border-b-black font-semibold">Details</p>
-                  <h3 className='my-2'><span className='font-semibold'>Type: </span>{move.type} <span> - {move.subType}</span></h3>
-                  <div className="flex justify-between">
-                    <p><span className="font-semibold">Damage: </span>{move.damage}</p>
-                    <p className="capitalize"><span className="font-semibold">Cost: </span>{move.cost.value} {move.cost.type}</p>
-                    <p><span className="font-semibold">Accuracy: </span>{move.accuracy}</p>
-                  </div>
-                  <h3 className="font-semibold">Effect: </h3>
-                  <p>{move.effect}</p>
-                  <p>{move.descriptionParams}</p>
-
-                </div>
-
-          </div>
-              ))}
-        </div> */}
       </div>
     </main>
   )
