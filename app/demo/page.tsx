cbe6f7f3e62b174b2afae7f5756a4edfb487dc86
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-04-21 02:44:36 -0600

    scaling back styling to reduce randomness

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -51,36 +51,34 @@ export default function page() {
       <>
             <p className='mb-4'>{Player.base.name}</p>
 
-            <div className='grid grid-cols-3 gap-2 mb-10'>
                   <div>
-                    <p className='text-center'>Sparking</p>
-                    <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                        {Player.sparkingMeter}
-                      </p>
-                  </div>
-
-                  <div>
-                      <p className='text-center'>Health</p>
                       <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                        {Player.hp}
+                        Health: {Player.hp}
                       </p>
                   </div>
 
+            <div className='grid grid-cols-2 gap-1'>
                   <div>
-                    <p className='text-center capitalize'>{Player.base.archetype} Counter</p>
                     <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                        {Player.archCounter}
+                        Sparking: {Player.sparkingMeter}
+                      </p>
+                  </div>
+
+
+                  <div>
+                    <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
+                    {Player.base.archetype} Counter: {Player.archCounter}
                       </p>
                   </div>
             </div>
 
-            <div className='grid grid-cols-3 gap-2'>
+            <div className='grid grid-cols-3 gap-1'>
 
-            <div className='grid grid-cols-2 col-span-2 grid-rows-3 gap-2 min-h-36 mb-4'>
+            <div className='grid grid-cols-2 col-span-2 grid-rows-3 gap-1 min-h-36 mb-4'>
 
                 {selectMenu === "base" && 
                 <>
-                  <p className='text-xl col-span-2'>What will you do?</p>
+                  <p className=' col-span-2'>What will you do?</p>
                 </>
                 }
                 {selectMenu === "action" && 
@@ -108,70 +106,67 @@ export default function page() {
             </div>
 
 
-            <div className='grid grid-cols-1 gap-2'>
-                
-                {selectMenu != "action" ? <button onClick={() => setSelectMenu("action")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ACTIONS</button> : <button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
-                
-                {selectMenu != "item" ? <button onClick={() => setSelectMenu("item")} className='bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ITEMS</button> :<button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
-                
-                {selectMenu != "ally" ? <button onClick={() => setSelectMenu("ally")} className='bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ALLIES</button> : <button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
-                
-                {/* {selectMenu != "base" && <button onClick={() => setSelectMenu("base")} className=' col-span-3 bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>Close</button>} */}
+              <div className='grid grid-cols-1 gap-1'>
+                  
+                  {selectMenu != "action" ? <button onClick={() => setSelectMenu("action")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ACTIONS</button> : <button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
+                  
+                  {selectMenu != "item" ? <button onClick={() => setSelectMenu("item")} className='bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ITEMS</button> :<button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
+                  
+                  {selectMenu != "ally" ? <button onClick={() => setSelectMenu("ally")} className='bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>ALLIES</button> : <button onClick={() => setSelectMenu("base")} className=' bg-blue-400 mb-2 px-2 py-1 rounded hover:text-white'>RETURN</button>}
 
-            </div>
+              </div>
             </div>
       </>
     )
   }
 
   return (
-    <div>
+    <div className=''>
       {/* <div className='bg-gray-100 rounded p-8'>
-        <p className='text-lg font-semibold text-center mb-4'>The Dungeons & DragonBalls Demo</p>
+        <p className=' font-semibold text-center mb-4'>The Dungeons & DragonBalls Demo</p>
         <p className='text-center'>Experience a one on one battle between the character Aspara and Turles!</p>
       </div> */}
 
       <div className='my-4 bg-white rounded p-4 grid grid-cols-1 '>
         <div className='bg-gray-500 border border-solid border-black rounded p-4'>
-          <div className='text-lg font-bold text-center bg-gray-400 border border-solid border-black p-2'>
-            <p className='mb-4 text-xl'>
+          <div className=' text-center bg-gray-400 border border-solid border-black p-2'>
+            <p className='mb-4'>
             {Boss.base?.displayName}
             </p>
-            <p className='font-semibold text-l mb-4'>Enemy state description.</p>
-            <div className='grid grid-cols-3 gap-2 mb-10'>
                 <div>
-                  <p className='text-center'>Sparking</p>
-                  <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                      {Boss.sparkingMeter}
-                    </p>
-                </div>
-                <div>
-                    <p className='text-center'>Health</p>
                     <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                      {Boss.hp}
+                      Health: {Boss.hp}
+                    </p>
+                </div>
+            <div className='grid grid-cols-2 gap-1'>
+                <div>
+                  <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
+                      Sparking: {Boss.sparkingMeter}
                     </p>
                 </div>
                 <div>
-                  <p className='text-center capitalize'>{Player.base.archetype} Counter</p>
                   <p className='bg-gray-700 border border-white text-white rounded px-2 py-1'>
-                      {Boss.archCounter}
+                  {Player.base.archetype} Counter: {Boss.archCounter}
                     </p>
                 </div>
             </div>
+            <div className='flex justify-center'>
+              <p>Distance: Far</p>
+            </div>
           </div>
-          <div className='my-2 text-center font-bold text-xl text-white'>
+          <div className='my-2 text-center text-white'>
             <div className='bg-black border border-white rounded px-2 py-2'>
-              <p className='text-lg'>characterName: Action</p>
+              <p className=''>characterName: Action</p>
               <p className='my-2'>
                 {`-[ Roll ]-`}
               </p>
-              <p className='text-lg'>Action Result: </p>
+              <p className=''>Action Result: </p>
               <p className='text-sm'>
                   Action result description
               </p>
             </div>
           </div>
-          <div className='text-lg font-bold text-center mt-4 bg-gray-300 border border-solid border-black p-2'>
+          <div className=' text-center mt-4 bg-gray-300 border border-solid border-black p-2'>
           
             <PlayerMenu/>
             
